12f24a021c9bbfa3fe7b6b9ee4bc87a40bcc7763
Francisco <ftarantuviez.dev@gmail.com>
2020-07-20 00:12:55 -0300

    styling

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import {Card, CardContent, Typography} from '@material-ui/core'
+import './Message.css'
 
 function Message(props) {
     return (
-        <div>
-            <Card>
-                <CardContent>
-                    <Typography
-                        color="white"
-                        variant="h5"
-                        component="h2"
-                    >
-                        {props.username}:{props.text}
-                    </Typography>
-                </CardContent>
-            </Card>
+    
+        <Card >
+            <CardContent>
+                <Typography
+                    color="white"
+                    variant="h5"
+                    component="h2"
+                >
+                    {props.username.length > 0 ? props.username : "Undefined user"}:{props.text}
+                </Typography>
+            </CardContent>
+        </Card>
             
-        </div>
+        
     )
 }
 

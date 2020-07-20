a05a3d46c7a227ed5dc5691abc284eb936348df1
Francisco <ftarantuviez.dev@gmail.com>
2020-07-19 23:30:45 -0300

    utton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Button} from '@material-ui/core'
 
-class App extends React.Component{
-    render(){
-        return(
-            <h1>This is an h1</h1>
-        )
+function App(){
+    const [input, setInput] = useState('')
+    const [messages, setMessages] = useState(["hello", "hi", "whats up"])
+
+    const sendMessage = (e) => {
+        e.preventDefault()
+        setMessages([...messages, input])
+        setInput('')
     }
+
+    return(
+        <div className="app">
+            <h1>This is a h1</h1>
+            
+            <form>
+                <input 
+                    type="text"
+                    onChange={event => setInput(event.target.value)}
+                    value={input}
+                />
+                <Button variant="contained" color="primary" onClick={sendMessage}>Send message</Button>
+
+            </form>
+            {
+                messages.map(message => (
+                    <p key={message}>{message}</p>
+                ))
+            }
+        </div>
+            
+    )
 }
 
 

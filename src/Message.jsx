8fef77a8c83d66ef5de471999b257d90e64bbf1a
Francisco <ftarantuviez.dev@gmail.com>
2020-07-20 00:08:37 -0300

    a

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Card, CardContent, Typography} from '@material-ui/core'
 
 function Message(props) {
     return (
-        <div className="message-container">
-            {props.username}: <h2>{props.text}</h2>
+        <div>
+            <Card>
+                <CardContent>
+                    <Typography
+                        color="white"
+                        variant="h5"
+                        component="h2"
+                    >
+                        {props.username}:{props.text}
+                    </Typography>
+                </CardContent>
+            </Card>
+            
         </div>
     )
 }

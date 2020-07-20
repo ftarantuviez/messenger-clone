12fa4bb19069acbc5a858b6bb9e0bbe2275606a1
Francisco <ftarantuviez.dev@gmail.com>
2020-07-20 10:43:51 -0300

    f

--- src/Message.jsx
+++ src/Message.jsx
@@ -7,20 +7,18 @@ function Message({message, username}) {
     const isUser = username === message.username;
 
     return (
-    
-        <Card className={`message ${isUser && 'message__user'}`}>
-            <CardContent>
-                <Typography
-                    color="white"
-                    variant="h5"
-                    component="h2"
-                >
-                    {message.username} :{message.text}
-                </Typography>
-            </CardContent>
-        </Card>
-            
-        
+        <div>
+            <Card className={`message ${isUser && 'message__user'}`}>
+                <CardContent className={isUser ? 'message__useCard' : "message__guestCard"}>
+                    <Typography
+                        variant="h5"
+                        component="h2"
+                    >
+                        {message.username} :{message.text}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>            
     )
 }
 
